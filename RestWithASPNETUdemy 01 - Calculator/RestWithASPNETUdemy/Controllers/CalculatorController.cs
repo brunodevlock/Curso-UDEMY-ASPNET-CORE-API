91762a9b2d692c35d65e402cef39ff8fafd1ac72
Bruno Andre Scatena <bandresc@everis.com>
2019-05-23 15:29:03 -0300

    start verbs REST

--- RestWithASPNETUdemy 01 - Calculator/RestWithASPNETUdemy/Controllers/CalculatorController.cs
+++ RestWithASPNETUdemy 01 - Calculator/RestWithASPNETUdemy/Controllers/CalculatorController.cs
@@ -9,26 +9,80 @@ namespace RestWithASPNETUdemy.Controllers
     [Route("api/[controller]")]
     public class CalculatorController : Controller
     {
-        
-
-        // GET api/values/5
-        [HttpGet("{firstNumber}/{secondtNumber}")]
+        //GET api/values/sum/5/5 
+        //Adição
+        [HttpGet("sum/{firstNumber}/{secondtNumber}")]
         public IActionResult Sum(string firstNumber, string secondNumber)
         {
-            if(IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
             {
                 var sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
                 return Ok(sum.ToString());
             }
 
             return BadRequest("Invalid Input");
+       }
+
+
+        //Subtração
+        [HttpGet("subtraction/{firstNumber}/{secondtNumber}")]
+        public IActionResult Subtraction(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sum = ConvertToDecimal(firstNumber) - ConvertToDecimal(secondNumber);
+                return Ok(sum.ToString());
+            }
+
+            return BadRequest("Invalid Input");
         }
+
+        //Multiplicação
+        [HttpGet("multiplication/{firstNumber}/{secondtNumber}")]
+        public IActionResult Multiplication(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sum = ConvertToDecimal(firstNumber) * ConvertToDecimal(secondNumber);
+                return Ok(sum.ToString());
+            }
+
+            return BadRequest("Invalid Input");
+        }
+
+       
+        //Média
+        [HttpGet("mean/{firstNumber}/{secondtNumber}")]
+        public IActionResult Mean(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var mean = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber) / 2;
+                return Ok(mean.ToString());
+            }
+
+            return BadRequest("Invalid Input");
+        }
+
+        //Square-root/5
+        [HttpGet("square-root/{number}")]
+        public IActionResult SquareRoot(string number)
+        {
+            if (IsNumeric (number))
+            {
+                var squareRoot = Math.Sqrt((double)ConvertToDecimal(number));
+                return Ok(squareRoot.ToString());
+            }
+
+            return BadRequest("Invalid Input");
+        }
+
 
         private decimal ConvertToDecimal(string number)
         {
             decimal decimalValue;
 
-            if  (decimal.TryParse(number, out decimalValue))
+            if (decimal.TryParse(number, out decimalValue))
             {
                 return decimalValue;
             }
@@ -41,5 +95,8 @@ namespace RestWithASPNETUdemy.Controllers
             bool isNumber = double.TryParse(strNumber, System.Globalization.NumberStyles.Any, System.Globalization.NumberFormatInfo.InvariantInfo, out number);
             return isNumber;
         }
+
+
+
     }
 }
